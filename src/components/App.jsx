8c8a700722125e5a8bcba1bada5dea7ecf0c52c7
Guilherme Silva <guilherme.gss@outlook.com.br>
2020-09-25 21:15:01 -0300

    add dinamic notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
-import Note from './components/Note';
-import Footer from './components/Footer';
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+import notes from '../notes';
 
 function App() {
   return (<div>
     <Header />
-    <Note />
+
+    {notes.map(note =>
+      <Note
+        title={note.title}
+        content={note.content}
+      />
+    )}
+
+
     <Footer />
 
   </div>
